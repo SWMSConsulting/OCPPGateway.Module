4dede28b9ca9441c179959ffce8087ecad6457de
ArnoSchiller <arno-schiller@hotmail.de>
2024-08-21 12:42:22 +0200

    moved models to module

---
+++ OCPPGateway.Module/Models/TransactionExtended.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OCPPGateway.Module.Models;
+public class TransactionExtended : Transaction
+{
+    public virtual string StartTagName { get; set; }
+    public virtual string StartTagParentId { get; set; }
+    public virtual string StopTagName { get; set; }
+    public virtual string StopTagParentId { get; set; }
+}
