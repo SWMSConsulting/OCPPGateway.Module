85a339ecf0dad4b957ff981a25b4d172c39aabd1
ArnoSchiller <arno-schiller@hotmail.de>
2024-09-25 10:35:08 +0200

    included change configuration

---
+++ OCPPGateway.Module/Messages_OCPP16/ChangeConfigurationRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace OCPPGateway.Module.Messages_OCPP16;
+
+public class ChangeConfigurationRequest
+{
+    [JsonProperty("key", Required = Required.Always)]
+    public string Key { get; set; }
+
+    [JsonProperty("value", Required = Required.Always)]
+    public string Value { get; set; }
+}
---
+++ OCPPGateway.Module/Messages_OCPP16/ChangeConfigurationResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace OCPPGateway.Module.Messages_OCPP16;
+public class ChangeConfigurationResponse
+{
+    [JsonProperty("status", Required = Required.Always)]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public ConfigurationStatus Status { get; set; }
+}
+
+public enum ConfigurationStatus
+{
+    [EnumMember(Value = @"Accepted")]
+    Accepted,
+
+    [EnumMember(Value = @"Rejected")]
+    Rejected,
+
+    [EnumMember(Value = @"RebootRequired")]
+    RebootRequired,
+
+    [EnumMember(Value = @"NotSupported")]
+    NotSupported
+}
