9450c31adc2f463902fd7f2112a763aaee4b6aaa
ArnoSchiller <arno-schiller@hotmail.de>
2024-10-11 14:32:59 +0200

    Added TriggerMessage support

---
+++ OCPPGateway.Module/Messages_OCPP16/TriggerMessageResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace OCPPGateway.Module.Messages_OCPP16;
+
+public class TriggerMessageResponse
+{
+    /// <summary>
+    /// Required. Indicates whether the request was accepted or rejected.
+    /// </summary>
+    [JsonProperty("status", Required = Required.Always)]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public TriggerMessageStatus Status { get; set; }
+}
+
+public enum TriggerMessageStatus
+{
+    [System.Runtime.Serialization.EnumMember(Value = @"Accepted")]
+    Accepted = 0,
+
+    [System.Runtime.Serialization.EnumMember(Value = @"Rejected")]
+    Rejected = 1,
+
+    [System.Runtime.Serialization.EnumMember(Value = @"NotImplemented")]
+    NotImplemented = 2
+}
