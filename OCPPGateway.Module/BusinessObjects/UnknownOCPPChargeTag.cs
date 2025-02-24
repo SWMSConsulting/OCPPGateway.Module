cc64d0fab61f2e99e5c1addc04e0e9895f7aa5f5
ArnoSchiller <arno-schiller@hotmail.de>
2025-02-24 11:10:35 +0100

    Added form to register unknown card

--- OCPPGateway.Module/BusinessObjects/UnknownOCPPChargeTag.cs
+++ OCPPGateway.Module/BusinessObjects/UnknownOCPPChargeTag.cs
@@ -13,27 +13,4 @@ public class UnknownOCPPChargeTag: BaseObject
 
     public virtual DateTime Timestamp { get; set; }
 
-
-    [Action(Caption = "Add to Charge Tags")]
-    public void AddToKnownChargeTags()
-    {
-        var type = OCPPChargeTag.AssignableType;
-        if (type == null)
-        {
-            return;
-        }
-
-        var existingChargeTag = (OCPPChargeTag)ObjectSpace.FindObject(type, CriteriaOperator.Parse("Identifier = ?", Identifier));
-
-        if (existingChargeTag == null)
-        {
-            existingChargeTag = (OCPPChargeTag)ObjectSpace.CreateObject(type);
-            existingChargeTag.Identifier = Identifier;
-            existingChargeTag.Name = Identifier;
-            existingChargeTag.Blocked = true;
-        }
-
-        ObjectSpace.Delete(this);
-        ObjectSpace.CommitChanges();
-    }
 }
---
+++ OCPPGateway.Module/Controllers/UnknownOCPPChargeTagViewController.cs
@@ -0,0 +1,61 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using InfluxDB.Client.Api.Domain;
+using OCPPGateway.Module.BusinessObjects;
+using System;
+
+namespace OCPPGateway.Module.Controllers;
+
+public class UnknownOCPPChargeTagViewController : ObjectViewController<ObjectView, UnknownOCPPChargeTag>
+{
+    public UnknownOCPPChargeTagViewController()
+    {
+        PopupWindowShowAction showPopUpAction = new PopupWindowShowAction(this, "Add to Charge Tags", "View")
+        {
+            ImageName = "Actions_Add",
+            SelectionDependencyType = SelectionDependencyType.RequireSingleObject
+        };
+        showPopUpAction.CustomizePopupWindowParams += showPopUpAction_CustomizePopupWindowParams;
+
+        
+    }
+
+
+    public void showPopUpAction_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+    {
+        var type = OCPPChargeTag.AssignableType;
+        if (type == null || ObjectSpace == null)
+        {
+            return;
+        }
+        var unknown = View.CurrentObject as UnknownOCPPChargeTag;
+        if(unknown == null || string.IsNullOrEmpty(unknown.Identifier))
+        {
+            return;
+        }
+
+        var identifier = unknown.Identifier;
+
+        var existingChargeTag = (OCPPChargeTag)ObjectSpace.FindObject(type, CriteriaOperator.Parse("Identifier = ?", identifier));
+
+        if (existingChargeTag == null)
+        {
+            existingChargeTag = (OCPPChargeTag)ObjectSpace.CreateObject(type);
+            existingChargeTag.Identifier = identifier;
+            existingChargeTag.Name = identifier;
+            existingChargeTag.Blocked = true;
+        }
+
+        ObjectSpace.Delete(unknown);
+        ObjectSpace.CommitChanges();
+        ObjectSpace.Refresh();
+
+        var newObjectSpace = Application.CreateObjectSpace<OCPPChargeTag>();
+        var chargeTag = newObjectSpace.FindObject(typeof(OCPPChargeTag), CriteriaOperator.Parse("Identifier = ?", identifier));
+        DetailView detailView = Application.CreateDetailView(newObjectSpace, chargeTag);
+        detailView.ViewEditMode = DevExpress.ExpressApp.Editors.ViewEditMode.Edit;
+        e.View = detailView;
+    }
+}
