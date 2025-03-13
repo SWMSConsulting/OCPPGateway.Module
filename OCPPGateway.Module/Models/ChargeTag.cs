a4013ab60ce323bb3800d7fda3ff259e751bd92b
ArnoSchiller <arno-schiller@hotmail.de>
2025-03-13 11:47:13 +0100

    included functions to convert from and to hex

--- OCPPGateway.Module/Models/ChargeTag.cs
+++ OCPPGateway.Module/Models/ChargeTag.cs
@@ -1,5 +1,7 @@
 ﻿#nullable disable
 
+using System.Buffers.Binary;
+
 namespace OCPPGateway.Module.Models;
 
 public partial class ChargeTag
@@ -17,4 +19,37 @@ public partial class ChargeTag
         else
             return TagId;
     }
+
+
+    public static string ToHexString(long tagId, bool reverseEndians)
+    {
+        if (reverseEndians)
+            return BinaryPrimitives.ReverseEndianness((uint)tagId).ToString("X");
+        else
+            return tagId.ToString("X");
+    }
+
+    public static long? FromHexString(string tagId, bool reverseEndians)
+    {
+        if (string.IsNullOrEmpty(tagId))
+            return null;
+
+        if (tagId.Count() == 12 && long.TryParse(tagId, out long result))
+        {
+            return result;
+        }
+
+        try
+        {
+            if (reverseEndians)
+                return BinaryPrimitives.ReverseEndianness(Convert.ToUInt32(tagId, 16));
+            else
+                return Convert.ToUInt32(tagId, 16);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
 }
